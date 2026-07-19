7edd90f63ad28a52c49d60de2bcb4a4979dafc38
AbdulSattar Majid <malikabdulsattarmajid@gmail.com>
2026-07-19 13:52:32 +0500

    Added recrutire stuff and the job thing and now fixed the error of s

--- routes/jobRoutes.js
+++ routes/jobRoutes.js
@@ -3,9 +3,8 @@ const router = express.Router();
 const { createJob, getJobs } = require('../controllers/jobController');
 
 
-const { protect } = require('../middlewares/authMiddleware'); 
+const  protect  = require('../middlewares/authMiddleware'); 
 
-// Apply the protect middleware so only logged-in users can access these routes
 router.route('/')
   .get(protect, getJobs)
   .post(protect, createJob);
